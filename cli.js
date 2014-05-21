763632094d29787359e1d45b86932547c3e58f79
johnny-ray <johnny@johnnyray.me>
2014-05-21 13:42:35 -0400

    demo

--- cli.js
+++ cli.js
@@ -3,15 +3,16 @@
 
 var args = require( 'minimist' )( process.argv.slice( 2 ) );
 var logger = require( 'luvely' );
-var app = require( './lib/gennifer' );
+var SocketManager = require( './lib/socketManager' );
+var utils = require( './lib/utils' );
 
 if ( !args.h ) {
 
-  var dataInterval;
   var frequency = args.f || 1000;
   var volume = args.v || 1;
   var port = args.p || 8080;
   var io = require( 'socket.io' ).listen( port );
+
   var tmpl = function() {
     return {
       when: Date.now()
@@ -20,39 +21,15 @@ if ( !args.h ) {
 
   logger.debug( 'Starting gennifer in server mode. Frequency: 1 tick/%sms, Volume: %s item(s)/tick', frequency, volume );
 
-  app.registerTemplate( 'time', tmpl );
-
   io.sockets.on('connection', function (socket) {
     logger.debug( 'connection' );
-
-    var reset = function() {
-      dataInterval = setInterval(function(){
-        app.volume( volume ).usingChannel( socket ).generate( 'time' );
-      }, frequency);
+    var opts = {
+      frequency: frequency,
+      volume: volume
     };
-
-    socket.on('configure', function(data){
-      clearInterval( dataInterval );
-      frequency = data.frequency || frequency;
-      volume = data.volume || volume;
-      logger.info( 'Client requested reconfiguration: ', data );
-      reset();
-    });
-
-    socket.on('disconnect', function(){
-      clearInterval( dataInterval );
-    });
-
-    reset();
-
+    new SocketManager( socket, opts ).setTemplate( 'time', tmpl ).run();
   });
 
 } else {
-  console.log('\n');
-  console.log( '    Gennifer Options' );
-  console.log('\n');
-  console.log( '    -f --frequency  [optional] How often (in milliseconds) to send new data - defaults to 1000ms' );
-  console.log( '    -v --volume     [optional] How many data items to send at each tick/interval - defaults to 1' );
-  console.log( '    -p --port       [optional] The port - defaults to 8080' );
-  console.log('\n');
+  utils.printHelp();
 }
